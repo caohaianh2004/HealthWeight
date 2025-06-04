78d35f99e06994c550b75d19523c36aecc01502e
caohaianh2004 <caohaianh26@gmail.com>
2025-06-04 11:12:30 +0700

    mmm

---
+++ Health_WeightApp.swift
@@ -0,0 +1,17 @@
+//
+//  Health_WeightApp.swift
+//  Health_Weight
+//
+//  Created by Boss on 04/06/2025.
+//
+
+import SwiftUI
+
+@main
+struct Health_WeightApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
